bbfebfd758787d1ff132095958dde50175bc8976
simonFCO <simon.vare6@gmail.com>
2026-02-01 19:50:15 +0400

    Add project files.

---
+++ SchoolProjectDatabase/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolProjectDatabase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
